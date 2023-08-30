faaab8ad82a2a59da1cb5ae12655d4709b5f83c4
smartful <remi_rodrigues@hotmail.com>
2023-08-31 01:32:52 +0200

    more feature on Register components

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,21 +1,54 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 
 export default function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("All fields are mandatory.");
+      return;
+    }
+  };
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-600">
         <h1 className="text-xl font-bold my-4">Register</h1>
-        <form className="flex flex-col gap-3">
-          <input type="text" placeholder="Full Name" />
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Full Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
             Register
           </button>
 
-          <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-            Error message
-          </div>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
 
           <Link href={"/"} className="text-sm mt-3 text-right">
             Already have an account? <span className="underline">Login</span>
